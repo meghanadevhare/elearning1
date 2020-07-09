6dd66cb50d77b62702cde014ac3c82a5ba1fcad0
meghana <meghanadevhare01@gmail.com>
2020-07-09 00:21:22 -0700

    first commit

---
+++ src/test/java/com/telusko/SpringBootRestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.telusko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("error???");
+	}
+
+}
